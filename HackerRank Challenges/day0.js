66d63e3b4296cb9827d89597a21b48c22c90586e
Carina Cruz <lecruz@bu.edu>
2018-03-30 00:51:35 +0000

    first commit

---
+++ HackerRank Challenges/day0.js
@@ -0,0 +1,13 @@
+// To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line. 
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log(inputString);
+    
+    // Write the second line of output that prints the contents of 'inputString' here.
+}   console.log("Hello, World.")
+
+
+
+// Input: Welcome to 30 Days of Code!
+// Sample Output: Hello, World. Welcome to 30 Days of Code!
