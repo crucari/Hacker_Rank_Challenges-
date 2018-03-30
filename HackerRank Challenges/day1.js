fdfc2639131e033a9874fc04ef053564deff5ef5
Carina Cruz <lecruz@bu.edu>
2018-03-30 01:00:02 +0000

    Data Types

---
+++ HackerRank Challenges/day1.js
@@ -0,0 +1,55 @@
+/*global readLine*/ 
+/*global d*/ 
+/*global i*/ 
+/*global s*/ 
+
+    // Declare second integer, double, and String variables.
+var x = "";
+
+var y = "";
+
+var z = "";
+
+// Read and save an integer, double, and String to your variables.
+
+x = parseInt(readLine()) + i;
+
+y = parseFloat(readLine()) + d;
+
+z = s + readLine();
+
+// Print the sum of both integer variables on a new line.
+
+console.log(x);
+
+// Print the sum of the double variables on a new line.
+
+console.log(y.toFixed(1));
+
+// Concatenate and print the String variables on a new line
+
+// The 's' variable above should be printed first.
+
+console.log(z);
+
+// Task:
+
+// Complete the code in the editor below. The variables i, d , and s are already declared and initialized for you. You must:
+
+// Declare  variables: one of type int, one of type double, and one of type String.
+// Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+// Use the  operator to perform the following operations: 
+// Print the sum of  plus your int variable on a new line.
+// Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+// Concatenate  with the string you read as input and print the result on a new line.
+
+
+// Sample Input
+// 12
+// 4.0
+// is the best place to learn and practice coding!
+
+// Sample Output
+// 16
+// 8.0
+// HackerRank is the best place to learn and practice coding!
